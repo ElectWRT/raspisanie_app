7d62c7e405441e22cc00475b9dbe9ae87afb162d
ElectWRT <pro100mavic@gmail.com>
2026-09-22 18:18:39 +1000

    Версия 1.7.0
    Осенняя иконка и окно благодарности.

--- lib/core/app_info.dart
+++ lib/core/app_info.dart
@@ -4,7 +4,7 @@ class AppInfo {
 
   /// Должна совпадать с `version:` в pubspec.yaml.
   /// За этим следит тест `test/app_info_test.dart`.
-  static const version = '1.6.0';
+  static const version = '1.7.0';
 
   static const repositoryUrl = 'https://github.com/ElectWRT/raspisanie_app';
 
